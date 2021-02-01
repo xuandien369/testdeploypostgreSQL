66a2d4e10f4efb360da7fe9f1c2259cc8360e956
xuandien369 <xuandien369@gmail.com>
2021-02-01 18:39:47 +0700

    push lan 2
    push lan 2

--- postgreSQL/src/main/java/com/example/postgreSQL/DemoPostgreSqlApplication.java
+++ postgreSQL/src/main/java/com/example/postgreSQL/DemoPostgreSqlApplication.java
@@ -1,13 +1,16 @@
 package com.example.postgreSQL;
 
-import com.example.postgreSQL.entity.Student;
-import com.example.postgreSQL.reponsitory.StudentRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.postgreSQL.reponsitory.StudentRes;
 
 @SpringBootApplication
+@RestController
 public class DemoPostgreSqlApplication implements CommandLineRunner {
 
 	@Autowired
@@ -19,11 +22,14 @@ public class DemoPostgreSqlApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Student student = res.findById(1).get();
-		student.setName("Ti");
-		res.save(student);
-
+//		Student student = res.findById(1).get();
+//		student.setName("Luan");
+//		res.save(student);
 //		res.save(new Student("Teo"));
-
+	}
+	@GetMapping(value = "/test")
+	public String getContent() {
+		
+		return res.findById(1).get().getName();
 	}
 }
